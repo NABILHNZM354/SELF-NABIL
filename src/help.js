146f79b02d735638fe6a7aa84dbed6d5a5c97512
NABILHNZM354 <76636460+NABILHNZM354@users.noreply.github.com>
2021-01-18 21:04:58 +0700

    MY BAILEYS

---
+++ src/help.js
@@ -0,0 +1,26 @@
+const help = (prefix) => {
+	return `┌──「 *SELF-BOT* 」
+│
+├「 *AUTHOR : NABIL* 」
+├「 *VERSION : 1.0* 」
+│
+├──「 *MENU* 」
+│
+├ *${prefix}runtime*
+├ *${prefix}add 62xxx*
+├ *${prefix}kick 62xx*
+├ *${prefix}cr1 @tag text|text*
+├ *${prefix}cr2 (versi private)*
+├ *${prefix}sticker*
+├ *${prefix}toimg*
+├ *${prefix}tsticker*
+├ *${prefix}memeindo*
+├ *${prefix}meme*
+├ *${prefix}ocr*
+├ *${prefix}tagall*
+├ *${prefix}info*
+│
+└──「 *SELF-BOT* 」`
+}
+
+exports.help = help
